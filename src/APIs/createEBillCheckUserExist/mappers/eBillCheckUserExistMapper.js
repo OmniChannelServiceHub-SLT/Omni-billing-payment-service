cba709d8bd7a3834fd8a523481887dcd054815d7
Kumudu Kavindya <kumudugamage2001@gmail.com>
2026-09-14 11:53:57 +0530

    Add eBill user check response mapper

---
+++ src/APIs/createEBillCheckUserExist/mappers/eBillCheckUserExistMapper.js
@@ -0,0 +1,74 @@
+// src/APIs/createEBillCheckUserExist/mappers/eBillCheckUserExistMapper.js
+
+const CUSTOMER_BILL_BASE_PATH =
+  '/tmf-api/customerBillManagement/v4/customerBill';
+
+/**
+ * Exact legacy dataBundle from Excel sheet "24".
+ */
+function toLegacyDataBundle(record) {
+  return {
+    userexist: record.userexist,
+    referenceNumber: record.referenceNumber,
+  };
+}
+
+/**
+ * Convert the record into a TMF-aligned CustomerBill resource.
+ */
+function toTmfCustomerBill(record) {
+  const id = String(record._id);
+
+  return {
+    id,
+
+    href: `${CUSTOMER_BILL_BASE_PATH}/${id}`,
+
+    billingAccount: {
+      id: record.accountNo,
+    },
+
+    relatedParty: [
+      {
+        id: record.tpNo,
+        role: 'subscriber',
+        '@referredType': 'Individual',
+      },
+    ],
+
+    contactMedium: [
+      {
+        mediumType: record.econtactType,
+        characteristic: {
+          contactType: record.econtactType,
+          emailAddress:
+            record.econtactType === 'EMAIL'
+              ? record.econtact
+              : undefined,
+          phoneNumber:
+            record.econtactType !== 'EMAIL'
+              ? record.econtact
+              : undefined,
+        },
+      },
+    ],
+
+    characteristic: [
+      {
+        name: 'userExists',
+        value: record.userexist === 'Y',
+      },
+      {
+        name: 'referenceNumber',
+        value: record.referenceNumber,
+      },
+    ],
+
+    '@type': 'CustomerBill',
+  };
+}
+
+module.exports = {
+  toLegacyDataBundle,
+  toTmfCustomerBill,
+};
