7ce95f51e3692e01b7e35adb156a8f674fe2c067
Kumudu Kavindya <kumudugamage2001@gmail.com>
2026-09-16 13:47:04 +0530

    Add EBillDownloadRequest response mapper

---
+++ src/APIs/createEBillDownloadRequest/mappers/eBillDownloadRequestMapper.js
@@ -0,0 +1,69 @@
+// src/APIs/createEBillDownloadRequest/mappers/eBillDownloadRequestMapper.js
+
+const CUSTOMER_BILL_BASE_PATH =
+  '/tmf-api/customerBillManagement/v4/customerBill';
+
+function toLegacyDataBundle() {
+  return null;
+}
+
+function toTmfCustomerBill(record) {
+  const id = String(record._id);
+
+  const response = {
+    id,
+
+    href: `${CUSTOMER_BILL_BASE_PATH}/${id}`,
+
+    billingAccount: {
+      id: record.accountNo,
+    },
+
+    relatedParty: [
+      {
+        id: record.tpNo,
+        role: 'subscriber',
+        '@referredType': 'Individual',
+      },
+    ],
+
+    characteristic: [
+      {
+        name: 'ebillMonth',
+        value: record.ebillMonth,
+      },
+      {
+        name: 'requestType',
+        value: 'download',
+      },
+    ],
+
+    state: record.downloadStatus,
+
+    '@type': 'CustomerBill',
+  };
+
+  if (record.eContact) {
+    const isEmail = record.eContact.includes('@');
+
+    response.contactMedium = [
+      {
+        mediumType: isEmail ? 'EMAIL' : 'PHONE',
+        characteristic: isEmail
+          ? {
+              emailAddress: record.eContact,
+            }
+          : {
+              phoneNumber: record.eContact,
+            },
+      },
+    ];
+  }
+
+  return response;
+}
+
+module.exports = {
+  toLegacyDataBundle,
+  toTmfCustomerBill,
+};
