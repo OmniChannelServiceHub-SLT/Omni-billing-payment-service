51c8804b1dcc27d2bb7aa105a4145cf09a76624d
Kumudu Kavindya <kumudugamage2001@gmail.com>
2026-09-16 18:14:46 +0530

    Add SaveInvoice response mapper

---
+++ src/APIs/createSaveInvoice/mappers/saveInvoiceMapper.js
@@ -0,0 +1,57 @@
+function toLegacyResponse() {
+  return {
+    isSuccess: true,
+    errorMessege: 'Successfully Saved.',
+    exceptionDetail: null,
+    dataBundle: null,
+    errorShow: 'Successfully Saved.',
+    errorCode: null,
+  };
+}
+
+function toTmfCustomerBill(record) {
+  const id = String(record._id);
+
+  return {
+    id,
+    href:
+      `/tmf-api/customerBillManagement/v4/customerBill/${id}`,
+    characteristic: [
+      {
+        name: 'referenceNumber',
+        value: record.refNo,
+      },
+      {
+        name: 'serviceType',
+        value: record.serviceType,
+      },
+      {
+        name: 'packageName',
+        value: record.packageName,
+      },
+      {
+        name: 'packageCount',
+        value: record.packageCount,
+      },
+      {
+        name: 'rental',
+        value: record.rental,
+      },
+      {
+        name: 'initialCharge',
+        value: record.initialCharge,
+      },
+      {
+        name: 'invoiceStatus',
+        value: record.status,
+      },
+    ],
+    state: 'saved',
+    '@type': 'CustomerBill',
+  };
+}
+
+module.exports = {
+  toLegacyResponse,
+  toTmfCustomerBill,
+};
