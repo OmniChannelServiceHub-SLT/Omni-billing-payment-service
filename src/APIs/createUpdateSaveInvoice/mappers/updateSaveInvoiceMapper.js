0db3c9d22fc12b8cbb4aaad0a46bb5032e16bcff
Kumudu Kavindya <kumudugamage2001@gmail.com>
2026-09-16 20:30:08 +0530

    Add UpdateSaveInvoice response mapper

---
+++ src/APIs/createUpdateSaveInvoice/mappers/updateSaveInvoiceMapper.js
@@ -0,0 +1,105 @@
+function toLegacyResponse() {
+  return {
+    isSuccess: true,
+    errorMessege: 'Successfully Updated.',
+    exceptionDetail: null,
+    dataBundle: null,
+    errorShow: 'Successfully Updated.',
+    errorCode: null,
+  };
+}
+
+function toCamelCase(value) {
+  return value
+    .toLowerCase()
+    .replace(
+      /_([a-z])/g,
+      (_, letter) => letter.toUpperCase()
+    );
+}
+
+function toTmfCustomerBill(record) {
+  const id = String(record._id);
+  const data = record.invoiceData || {};
+
+  const partyId =
+    data.NIC ||
+    data.SLTNIC ||
+    data.nic ||
+    data.sltnic ||
+    '';
+
+  const email =
+    data.EMAIL || data.email || '';
+
+  const phone =
+    data.MOBILENO ||
+    data.mobileNo ||
+    data.mobileno ||
+    '';
+
+  const excludedKeys = new Set([
+    'EMAIL',
+    'email',
+    'MOBILENO',
+    'mobileNo',
+    'mobileno',
+  ]);
+
+  const characteristic = [
+    {
+      name: 'referenceNumber',
+      value: record.refNo,
+    },
+    ...Object.entries(data)
+      .filter(
+        ([key, value]) =>
+          !excludedKeys.has(key) &&
+          value !== undefined &&
+          value !== null &&
+          value !== ''
+      )
+      .map(([key, value]) => ({
+        name: toCamelCase(key),
+        value,
+      })),
+  ];
+
+  const response = {
+    id,
+    href:
+      `/tmf-api/customerBillManagement/v4/customerBill/${id}`,
+    characteristic,
+    state: record.updateStatus,
+    '@type': 'CustomerBill',
+  };
+
+  if (partyId) {
+    response.relatedParty = [
+      {
+        id: String(partyId),
+        role: 'customer',
+        '@referredType': 'Individual',
+      },
+    ];
+  }
+
+  if (email || phone) {
+    response.contactMedium = [
+      {
+        mediumType: email ? 'EMAIL' : 'PHONE',
+        characteristic: {
+          emailAddress: email,
+          phoneNumber: phone,
+        },
+      },
+    ];
+  }
+
+  return response;
+}
+
+module.exports = {
+  toLegacyResponse,
+  toTmfCustomerBill,
+};
