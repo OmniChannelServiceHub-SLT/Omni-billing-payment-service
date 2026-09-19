df3d2242ea2e1f94de23e06b6d2d3e68f4b7fadd
Kumudu Kavindya <kumudugamage2001@gmail.com>
2026-09-19 12:46:46 +0530

    Add UpdatePaymentLogs response mapper

---
+++ src/APIs/createUpdatePaymentLogs/mappers/updatePaymentLogsMapper.js
@@ -0,0 +1,23 @@
+const {
+  toTmfPayment,
+} = require('../../createGetPaymentLogs/mappers/getPaymentLogsMapper');
+
+function toLegacyResponse() {
+  return {
+    isSuccess: true,
+    errorMessege: 'Successfully Updated.',
+    exceptionDetail: null,
+    dataBundle: null,
+    errorShow: 'Successfully Updated.',
+    errorCode: null,
+  };
+}
+
+function toTmfResponse(records) {
+  return records.map(toTmfPayment);
+}
+
+module.exports = {
+  toLegacyResponse,
+  toTmfResponse,
+};
